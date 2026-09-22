4fd8658c8a4abd622a7cbf4a0ad7daed10757b38
realP1406 <paramsewanihingorani1406@gmail.com>
2026-09-22 09:04:37 +0100

    fear: added next payment amount calculation

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
@@ -9,18 +9,30 @@ public class main {
         List<Integer> payMonths = new ArrayList<>();
 
         payMonths.add(1);
+        payMonths.add(3);
         payMonths.add(4);
+        payMonths.add(5);
+        payMonths.add(6);
+        payMonths.add(7);
+        payMonths.add(8);
+        payMonths.add(9);
         payMonths.add(10);
+        payMonths.add(11);
+        payMonths.add(12);
 
         Bonds testBond = new Bonds(
             "Bond A",
             LocalDate.of(2030, 12, 31),
-            15,
+            29,
             payMonths,
-            false
+            false,
+            1000,
+            9.75
         );
 
-        System.out.println(testBond.getNextPaymentDate());
+        System.out.println("Next Payment Date: " + testBond.getNextPaymentDate());
+        System.out.println("Last Payment Date: " + testBond.getLastPayDate());
+        System.out.println("Next Payment Amount: " + testBond.getNextPaymentAmount());
     }
 }
 
@@ -31,19 +43,25 @@ class Bonds {
     private int payDay;
     private List<Integer> payMonths = new ArrayList<>();
     private Boolean isCumulative;
+    private int faceValue;
+    private double couponRate;
 
     public Bonds(
         String bondName,
         LocalDate maturityDate,
         int payDay,
         List<Integer> payMonths,
-        Boolean isCumulative
+        Boolean isCumulative,
+        int faceValue,
+        double couponRate
     ) {
         this.bondName = bondName;
         this.maturityDate = maturityDate;
         this.payDay = payDay;
         this.payMonths = payMonths;
         this.isCumulative = isCumulative;
+        this.faceValue = faceValue;
+        this.couponRate = couponRate;
     }
 
     public LocalDate getNextPaymentDate() {
@@ -82,5 +100,52 @@ class Bonds {
         }
 
         return nextPaymentDate;
+    }
+
+    public LocalDate getLastPayDate() {
+        
+        LocalDate today = LocalDate.now();
+        LocalDate lastPaymentDate = null;
+
+        if (isCumulative) {
+            return maturityDate;
+        } else {
+
+            for (int i = payMonths.size() - 1; i >= 0; i--) {
+
+                int month = payMonths.get(i);
+                LocalDate paymentDate =
+                    LocalDate.of(today.getYear(), month, payDay);
+
+                if (paymentDate.isBefore(today)) {
+                    lastPaymentDate = paymentDate;
+                    break;
+                }
+            }
+
+            if (lastPaymentDate == null) {
+
+                for (int i = payMonths.size() - 1; i >= 0; i--) {
+
+                    int month = payMonths.get(i);
+                    LocalDate paymentDate =
+                        LocalDate.of(today.getYear() - 1, month, payDay);
+
+                    if (paymentDate.isBefore(today)) {
+                        lastPaymentDate = paymentDate;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return lastPaymentDate;
+    }
+
+    public Double getNextPaymentAmount() {
+        LocalDate nextPaymentDate = getNextPaymentDate();
+        LocalDate lastPaymentDate = getLastPayDate();
+        int diffInDays = (int) java.time.temporal.ChronoUnit.DAYS.between(lastPaymentDate, nextPaymentDate);
+        return (faceValue * (couponRate) * diffInDays / 365.0);
     }
 }
